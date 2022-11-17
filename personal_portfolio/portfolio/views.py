61c3dc942b749f21146b6a182d0394078a1864fe
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2022-11-17 21:53:25 +0600

    Initial commit

--- personal_portfolio/portfolio/views.py
+++ personal_portfolio/portfolio/views.py
@@ -3,5 +3,5 @@ from .models import Project
 
 
 def home(request):
-    projects = Project.objects.all(),
+    projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'projects': projects})
